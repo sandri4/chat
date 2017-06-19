dd5da24a866d213bea8dcb08d8d9bb0062022356
sandri4 <pletin1997@outlook.com>
2017-06-19 18:46:42 +0300

    fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#show'
 
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
 
   devise_scope :user do
     get 'profile', to: 'users/registrations#show'
--- db/migrate/20170619064723_add_fields_to_users.rb
+++ db/migrate/20170619064723_add_fields_to_users.rb
@@ -4,6 +4,6 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :birthday, :date
-    add_column :users, :gender, :integer, :default => 1
+    add_column :users, :gender, :integer
   end
 end
