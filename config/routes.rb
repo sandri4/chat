07118593fe27b85b194129b6a927b89d9786c61e
sandri4 <pletin1997@outlook.com>
2017-06-20 12:22:24 +0300

    ActionCable Chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  root 'home#show'
-
   devise_for :users, controllers: {registrations: 'users/registrations'}
+
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  resources :users
+
+
 
   devise_scope :user do
     get 'profile', to: 'users/registrations#show'
+    get 'profile_update', to: 'users/registrations#edit'
   end
-  # resources :users
+
+
+
+
+
+  root 'chat_rooms#index'
+  mount ActionCable.server => '/cable'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619155146) do
+ActiveRecord::Schema.define(version: 20170619221324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_chat_rooms_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "chat_room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,7 +50,7 @@ ActiveRecord::Schema.define(version: 20170619155146) do
     t.string "first_name"
     t.string "last_name"
     t.date "birthday"
-    t.integer "gender", default: 1
+    t.integer "gender"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
@@ -41,4 +59,7 @@ ActiveRecord::Schema.define(version: 20170619155146) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chat_rooms", "users"
+  add_foreign_key "messages", "chat_rooms"
+  add_foreign_key "messages", "users"
 end
