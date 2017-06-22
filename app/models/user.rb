8c9d5783e0a5e719e2a9988f13ddd68ae09527de
wolferen <veliar.master@gmail.com>
2017-06-22 04:02:19 +0300

    css2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: {small: '40x40', medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/defaultavatar.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :background_image, styles: { medium: "240x720>", thumb: "100x100>" }, default_url: "/images/back/:style/defaultback.jpg"
+  has_attached_file :background_image, styles: { medium: "240x720>", thumb: "100x100>" }, default_url: "/images/back/:style/defback.jpg"
   validates_attachment_content_type :background_image, content_type: /\Aimage\/.*\z/
   def name
     email.split('@')[0]
